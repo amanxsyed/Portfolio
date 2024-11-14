a30196327bd7376f507a238c7c8c94b00023abd6
Syed <amanxsyed@gmail.com>
2024-11-14 11:37:42 +0500

    Add TextGenerateEffect component and integrate it into Hero component

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 export const Hero = () => {
   return (
@@ -21,6 +22,7 @@ export const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase  tracking-widest text-xs text-center text-blue-100 max-w-80 ">Dynamic Web Magic with Next.js</h2>
+          <TextGenerateEffect words="Transforming Concepts into Seamless Experiences" className="text-[40px] md:text-5xl lg:text-6xl text-center" />
         </div>
       </div>
     </div>
