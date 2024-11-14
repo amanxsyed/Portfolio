701a6e708d55f44071bdf0404cc5b24b1dbbfce4
Syed <amanxsyed@gmail.com>
2024-11-14 09:35:32 +0500

    refactor: enhance layout and styling for home page; update Tailwind CSS color configuration

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="relative">
-      <div>
+    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-10">
+      <div className="max-w-7xl w-full">
         <h1>Hello Portfolio</h1>
       </div>
     </main>
