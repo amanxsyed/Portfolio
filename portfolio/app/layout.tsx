3f720d25622907466ab46273bc2015783381188e
Syed <amanxsyed@gmail.com>
2024-11-14 11:09:23 +0500

    implement theme provider and enhance styling; add new utility classes and update Hero component

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Syed Aman's Portfolio",
-  description: "Modern & Minimal Portfolio",  
+  description: "Modern & Minimal Portfolio",
 };
 
 export default function RootLayout({
@@ -21,7 +21,14 @@ export default function RootLayout({
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
