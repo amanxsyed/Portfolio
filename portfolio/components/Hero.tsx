c23c6744bae2972b5a0a7a253accda1fba8e573f
Syed <amanxsyed@gmail.com>
2024-11-15 21:34:29 +0500

    add introduction paragraph to Hero component and update TextGenerateEffect styling

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -23,6 +23,7 @@ export const Hero = () => {
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase  tracking-widest text-xs text-center text-blue-100 max-w-80 ">Dynamic Web Magic with Next.js</h2>
           <TextGenerateEffect words="Transforming Concepts into Seamless Experiences" className="text-[40px] md:text-5xl lg:text-6xl text-center" />
+          <p className="text-center md:tracking-wider mb-4 text-sm mb:text-lg lg:text-2xl ">Hi, I&apos;m Syed Aman, a Next.js developer based in Pakistan</p>
         </div>
       </div>
     </div>
