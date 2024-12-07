c2d261580bdf5cc14946455fb58cf8af457dbb7a
Syed <amanxsyed@gmail.com>
2024-12-07 14:23:36 +0500

    Refactor RecentProjects component: adjust layout and improve responsiveness

--- portfolio/components/RecentProjects.tsx
+++ portfolio/components/RecentProjects.tsx
@@ -8,14 +8,14 @@ const RecentProjects = () => {
     <div className="py-20">
       <h1 className="heading">A small selection of </h1>
       <span className="text-purple">recent projects</span>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10 ">
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] mb-10 sm:h-[40vh] ">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
